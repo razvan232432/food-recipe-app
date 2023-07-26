1b92056f3c840f0801b4a9346dbf7886feb235f8
razvan232432 <marius3837@proton.me>
2023-07-26 09:37:22 -0700

    Update App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import { useState, useEffect } from 'react';
+import './App.css';
+import SearchBar from './components/SearchBar';
+import RecipeCard from './components/RecipeCard';
+
+const apiUrl = 'www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata';
+
+function App() {
+  return (
+    <div className="container">
+    </div>
+  );
+}
+
+export default App;
