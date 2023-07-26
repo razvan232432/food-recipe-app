496ec25a172abde4895767829d4be3897a5092a1
razvan232432 <marius3837@proton.me>
2023-07-26 10:32:06 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
-import { useState, useEffect } from 'react';
-import './App.css';
-import SearchBar from './components/SearchBar';
-import RecipeCard from './components/RecipeCard';
-
-const apiUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import SearchBar from "./components/SearchBar";
+import RecipeCard from "./components/RecipeCard";
+const searchApi = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  
+  // search for the recipe
+  const searchRecipes = async () => {
+    setIsLoading(true);
+    const url = searchApi + query
+    const res = await fetch(url);
+    const data = await res.json();
+    setRecipes(data.meals);
+    setIsLoading(false);
+  };
 
-  const [isLoadin, setIsLoadind] = useState(false)
-  const [query, setQuery] = useState('')
-  const [recipe, setRecipe] = useState([])
+  useEffect(() => {
+    searchRecipes()
+  }, []);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    searchRecipes();
+  }
+
   return (
     <div className="container">
-        <h2>Our Recipe App</h2>
+      <h2>Our Food Recipes</h2>
+      <SearchBar
+        isLoading={isLoading}
+        query={query}
+        setQuery={setQuery}
+        handleSubmit={handleSubmit}
+      />
+      <div className="recipes">
+        
+        {recipes ? recipes.map(recipe => (
+          <RecipeCard
+             key={recipe.idMeal}
+             recipe={recipe}
+          />
+        )) : "No Results."}
+      </div>
     </div>
   );
 }
