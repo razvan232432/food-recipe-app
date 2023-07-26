08b0ca3f5249172535c35e22eecf57861393353c
razvan232432 <marius3837@proton.me>
2023-07-26 09:48:23 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,11 +3,16 @@ import './App.css';
 import SearchBar from './components/SearchBar';
 import RecipeCard from './components/RecipeCard';
 
-const apiUrl = 'www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata';
+const apiUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=';
 
 function App() {
+
+  const [isLoadin, setIsLoadind] = useState(false)
+  const [query, setQuery] = useState('')
+  const [recipe, setRecipe] = useState([])
   return (
     <div className="container">
+        <h2>Our Recipe App</h2>
     </div>
   );
 }
